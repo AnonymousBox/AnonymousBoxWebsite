a1f535a6097d76590dd5b36de7bf96779bd78dbe
Jake Schievink <jakeschievink@gmail.com>
2014-04-16 13:12:40 -0400

    added testing

--- public/javascripts/styling.js
+++ public/javascripts/styling.js
@@ -1,7 +1,7 @@
 
 $(function(){
     $(document.body).css('padding-top', $('#topnavbar').height() + 40);
-    $('.rimg').click( function(){
+    $(".rimg").click( function(){
        console.log("clicked");
        $(this).animate({width: "500px", height: "500px"},500);
     });
