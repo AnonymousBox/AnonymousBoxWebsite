294ead08810b3d3139a32ea21cc8b6598f120b21
Jake Schievink <jakeschievink@gmail.com>
2014-04-16 12:45:47 -0400

    trying to add animation

--- public/javascripts/styling.js
+++ public/javascripts/styling.js
@@ -1,4 +1,9 @@
 
 $(function(){
     $(document.body).css('padding-top', $('#topnavbar').height() + 40);
+    var bindClicks = function(){
+        $('.rimg').bind("click", function(){
+           $('.rimg').animate({width: "500px", height: "500px"},500);
+        });
+    }
 });
